7b5048b2025d648735955b8fce112b6fde05a4a0
rini <rinisolomon@hotmail.com>
2016-09-05 00:49:21 -0700

    Main Program

--- DataStructures/DataStructures/src/LinkedList/Program.java
+++ DataStructures/DataStructures/src/LinkedList/Program.java
@@ -8,9 +8,9 @@ public class Program {
 		SingleLinkedList s = new SingleLinkedList();
         for (int i=0;i<5;i++)
         {
-		System.out.println("enter value");
-		Scanner r = new Scanner(System.in);
-		s.AddNode(r.nextInt());
+        	System.out.println("enter value");
+        	Scanner r = new Scanner(System.in);
+        	s.AddNode(r.nextInt());
 		
         }
         
