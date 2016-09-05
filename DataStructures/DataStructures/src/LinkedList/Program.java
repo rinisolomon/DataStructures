a9e4c21d6604650472c0e57a4fd892a3b566fbd5
rinisolomon <rinisolomon@hotmail.com>
2016-09-05 12:31:45 -0700

    Implemented methods
    1.Insert node in beginning, end , in the middle
    2. Delete node in the end , beginnign, middle.

--- DataStructures/DataStructures/src/LinkedList/Program.java
+++ DataStructures/DataStructures/src/LinkedList/Program.java
@@ -13,11 +13,51 @@ public class Program {
         	s.AddNode(r.nextInt());
 		
         }
-        
         s.PrintElements();
         
         
-		
+   /*     //insert node in the beginning of the linked list
+        System.out.println("enter value to insert in the beginning");
+    	Scanner r = new Scanner(System.in);
+        s.AddNodeFirst(r.nextInt());
+        s.PrintElements(); */
+        
+        
+     /*   //insert node at the end of the linked list
+        System.out.println("enter value to insert in the end of the linked list");
+    	Scanner r1 = new Scanner(System.in);
+        s. AddNodeLast(r1.nextInt());
+        s.PrintElements(); */
+        
+       int choice =0;
+       /*
+        do
+        {
+        System.out.println("Enter the position to enter");
+        Scanner r2 = new Scanner(System.in);
+        int pos = r2.nextInt();
+        System.out.println("Enetr the value to insert");
+        Scanner r3 = new Scanner(System.in);
+        int val = r3.nextInt();
+        
+        s.AddNode(val,pos);
+        s.PrintElements();
+        
+        System.out.println("Do you want to continue 0 -> Continue  1 -->Exit");
+        Scanner r4 = new Scanner(System.in);
+        choice = r4.nextInt();
+        }while(choice == 0);     */
+
+	   do
+	   {
+		   s.RemoveNode();
+		   s.PrintElements();
+		   System.out.println("Do you want to continue 0 -> Continue  1 -->Exit");
+	       Scanner r4 = new Scanner(System.in);
+	       choice = r4.nextInt();
+	      
+	    }while(choice == 0);  
+	
 	}
 
 }
--- DataStructures/DataStructures/src/LinkedList/SingleLinkedList.java
+++ DataStructures/DataStructures/src/LinkedList/SingleLinkedList.java
@@ -26,14 +26,131 @@ public class SingleLinkedList
 		count++;
 	}
 	
+	public void AddNodeFirst(int value)
+	{
+		Node n = new Node(value);
+		if (count == 0)
+		{
+			Head = n;
+			Tail = n;
+		}
+		else
+		{
+			n.SetNext(Head);
+			Head = n ;
+		}
+		count++;
+	}
+	
+	
+	public void AddNodeLast(int value)
+	{
+		Node newnode = new Node(value);
+		if (count==0)
+		{
+			
+			Head = newnode ;
+			Tail = newnode ;
+			count++;
+		}
+		else
+		{
+			Tail.SetNext(newnode);;
+			Tail = newnode ;
+			count++;
+		}
+	}
+	
+	
+	public void AddNode(int value , int position)
+	{
+		if ((position) >(count+1))     // if given position is out of bounds
+		
+		{
+			throw new IllegalArgumentException("item out of bounds");
+		}
+		else
+		{
+			if ((position) == (count+1))  // inserting a node in the last
+			{
+				AddNodeLast( value);
+			}
+			else if ((position) == 1)  // inserting a node in the beginning
+			{
+				AddNodeFirst(value);
+			}
+			else
+			{
+				Node Current = Head;       
+				for(int i=1;i<(position-1);i++)                 // loop till position is 1 less than count , so that current.getNext() will point to the node in t he given position.
+				{
+					Current = Current.GetNext();
+				}
+				
+				Node newnode = new Node(value);         // creating a new node with the given value
+				newnode.SetNext(Current.GetNext());     // set the node which the current node points to to what newnode should point to
+				Current.SetNext(newnode);    
+				count++;
+				                          //set the current node's next pointer to point to new node.
+				
+			}
+		}
+	}
+	
+	
+	public void RemoveNode()
+	{
+		System.out.println("Enter the position of node to remove ");
+		Scanner r = new Scanner(System.in);
+		int pos= r.nextInt();
+		Node Next;
+		Node Current =Head;
+		if (pos==1)
+		{
+			Head =Head.GetNext();
+			count--;
+		}
+		else if(pos ==count)
+		{
+			for (int i =1;i<pos-1;i++)
+			{
+				Current =Current.GetNext();
+			}
+			Tail=Current;
+			Tail.SetNext(null);
+			count--;
+			
+		}
+		else if (pos == 0 || pos>count)
+		{
+			throw new IllegalArgumentException("wrong position entered");
+		}
+		else
+		{
+			for (int i =1;i<pos-1;i++)
+			{
+				Current =Current.GetNext();
+			}
+			Next =Current.GetNext();
+			Current.SetNext(Next.GetNext());
+			count--;
+		}
+			
+	}
+	
+	
+	
 	public void PrintElements()
 	{
 		Node Current = Head;
 		while(Current!=null)
 		{
-			System.out.println(Current.GetValue());
+			System.out.print(Current.GetValue() + " -> ");
 			Current=Current.GetNext();
 		}
+		System.out.print("null");
+        System.out.println(" ");
+        System.out.println("Count :" + count );
 	}
  
  
